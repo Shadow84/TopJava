1c44655acef43880a44261141290b02d49e3cfc8
Serhiy Khomenko <Khomenkosm@gmail.com>
2016-10-16 23:14:27 +0300

    HW5 optinal 2 done

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.util.TimeUtil;
 import ru.javawebinar.topjava.web.meal.UserMealRestController;
@@ -32,7 +34,10 @@ public class MealServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext = new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml", "spring/spring-db.xml"}, false);
+        ConfigurableEnvironment environment = springContext.getEnvironment();
+        environment.setActiveProfiles(Profiles.ACTIVE_DB, Profiles.ACTIVE_REPOSITORY);
+        springContext.refresh();
         mealController = springContext.getBean(UserMealRestController.class);
     }
 
