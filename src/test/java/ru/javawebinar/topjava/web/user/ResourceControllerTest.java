a488c64b971263cf4515e7fcd53d653ea400a42c
Serhiy Khomenko <Khomenkosm@gmail.com>
2016-11-06 16:50:20 +0200

    HW 7.1 done

---
+++ src/test/java/ru/javawebinar/topjava/web/user/ResourceControllerTest.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Privat on 06.11.2016.
+ */
+public class ResourceControllerTest extends AbstractControllerTest {
+    @Test
+    public void testResources() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.valueOf("text/css")))
+                .andExpect(status().isOk());
+    }
+}
